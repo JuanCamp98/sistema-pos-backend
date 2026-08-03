998843a7515b04195362a7a191612d880e7f1a65
pedrocortii <ped10ro11@gmail.com>
2026-08-02 22:11:17 -0300

    feat: aplica validacion de entrada en las rutas de los 4 modulos

--- src/routes/movimientoStockRoutes.js
+++ src/routes/movimientoStockRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const movimientoStockController = require("../controllers/movimientoStockController");
 const verificarToken = require("../middlewares/verificarToken");
 const verificarRol = require("../middlewares/verificarRol");
+const validar = require("../middlewares/validar");
+const { registrarMovimientoSchema } = require("../schemas/movimientoStockSchema");
 const router = express.Router();
 
-router.post("/", verificarToken, verificarRol(["Administrador"]), movimientoStockController.registrar);
+router.post("/", verificarToken, verificarRol(["Administrador"]), validar(registrarMovimientoSchema), movimientoStockController.registrar);
 router.get("/", verificarToken, movimientoStockController.listar);
 router.get("/producto/:productoId", verificarToken, movimientoStockController.listarPorProducto);
 
--- src/routes/ventaRoutes.js
+++ src/routes/ventaRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const ventaController = require("../controllers/ventaController");
 const verificarToken = require("../middlewares/verificarToken");
+const validar = require("../middlewares/validar");
+const { registrarVentaSchema } = require("../schemas/ventaSchema");
 const router = express.Router();
 
-router.post("/", verificarToken, ventaController.registrar);
+router.post("/", verificarToken, validar(registrarVentaSchema), ventaController.registrar);
 router.get("/", verificarToken, ventaController.listar);
 router.get("/:id", verificarToken, ventaController.obtenerPorId);
 
