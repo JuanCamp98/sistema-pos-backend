cfecf1f8f87e7852948182d3ac81a226532a53e4
pedrocortii <ped10ro11@gmail.com>
2026-08-14 23:19:01 -0300

    fix(errores): preservar codigos HTTP de Express

--- src/middlewares/manejadorErrores.js
+++ src/middlewares/manejadorErrores.js
@@ -1,7 +1,7 @@
 function manejadorErrores(error, req, res, next) {
     console.log(error);
-    const codigoEstado = error.codigoEstado || 500;
-    const mensaje = error.codigoEstado ? error.message : "Ocurrio un error en el servidor";
+    const codigoEstado = error.codigoEstado || error.status || 500;
+    const mensaje = error.codigoEstado || error.status ? error.message : "Ocurrio un error en el servidor";
     res.status(codigoEstado).json({ mensaje: mensaje });
 }
 
